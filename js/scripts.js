de1308fb85720d36a38b47e23804fc84b3d4d50a
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2021-10-24 19:55:28 -0500

    Removed previous code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,1 @@
-let myName = 'Kelsie';
-document.write(myName);
 
-myName = 'John Doe';
-document.write(myName);
-
-//this is a comment!
-
-let size = 100;
-let doubleSize = size * 2
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-let favoriteFood = 'Tamales';
-document.write(favoriteFood);

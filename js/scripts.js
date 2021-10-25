b11f002e7e52f7433ae2776e78d1f8cb203e5576
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2021-10-24 20:05:38 -0500

    Added variable list containing 4 pokemon and details

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-
+let pokemonList = [
+  {name:'Eevee', height:2, type: ['Normal']},
+  {name:'Chikorita', height:3, type: ['grass','poison']},
+  {name:'Haunter', height:1, type: ['ghost','poison']},
+  {name:'Geodude', height:1, type: ['ground']}
+];

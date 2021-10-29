c1fe70d1c8908ed3b1c9bb90f0748b65e2ac8f70
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2021-10-28 21:24:13 -0500

    Added Pokemon lists with loops to  javascript page and moved script into div

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,41 @@
 let pokemonList = [
-  {name:'Eevee', height:2, type: ['Normal']},
-  {name:'Chikorita', height:3, type: ['grass','poison']},
-  {name:'Haunter', height:1, type: ['ghost','poison']},
-  {name:'Geodude', height:1, type: ['ground']}
+  {
+    name:'Eevee',
+    height:1.5,
+    type: ['Normal']
+  },
+  {
+    name:'Meganium',
+    height:4.5,
+    type: ['grass','poison']
+  },
+  {
+    name:'Haunter',
+    height:1.2,
+    type: ['ghost','poison']
+  },
+  {
+    name:'Geodude',
+    height:0.7,
+    type: ['ground']
+  }
 ];
+let text = " ";
+let space = "<br>";
+
+
+for (let i=0; i < pokemonList.length; i++)
+//lists Pokemon name and their height
+    if (pokemonList) {
+      document.write(pokemonList[i].name + text + "height:" + pokemonList[i].height + space)
+    }
+
+for (let i=0; i < pokemonList.length; i++)
+// lists Pokemon name and their sizes relative to their height
+    if (pokemonList[i].height <2.0 && pokemonList[i].height >1.0){
+      document.write(pokemonList[i].name + " is a medium Pokemon!" + space)
+    }else if (pokemonList[i].height <1.0){
+      document.write(pokemonList[i].name + " is a small Pokemon!" + space)
+    }else {
+      document.write(pokemonList[i].name + " ...Wow! A very large Pokemon!" + space)
+    }
